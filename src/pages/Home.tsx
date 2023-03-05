3175e8849f0ec5dd1865f0109f6c240eac7a3103
Caique Almeida <caaguima@gmail.com>
2023-03-05 10:29:41 -0300

    feat: started table and home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+import ExpensesTable from "components/ExpensesTable";
+
 const Home = () => {
-	return <div data-testid="home-page">Home</div>;
+	return (
+		<div data-testid="home-page">
+			<Heading>Home</Heading>
+			<ExpensesTable />
+		</div>
+	);
 };
 
 export default Home;
